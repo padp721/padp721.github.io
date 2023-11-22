9f3fe5856661e3127919ba5eb80fc5dbba59f9fb
randomize721 <anggadp91@gmail.com>
2023-11-22 22:39:59 +0800

    new instagram background

--- src/components/FaIcon.vue
+++ src/components/FaIcon.vue
@@ -1,14 +1,40 @@
 <script setup lang="ts">
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faXTwitter, faFacebook, faInstagram, faGithub, faLinkedin, faTelegramPlane, faSpotify, faSteam, faThreads, faDiscord } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {
+    faXTwitter,
+    faFacebook,
+    faInstagram,
+    faGithub,
+    faLinkedin,
+    faTelegramPlane,
+    faSpotify,
+    faSteam,
+    faThreads,
+    faDiscord
+} from '@fortawesome/free-brands-svg-icons'
+import {
+    faEnvelope
+} from '@fortawesome/free-solid-svg-icons'
 
 defineProps(['type', 'icon', 'spin'])
 // Brands
-library.add(faXTwitter, faFacebook, faInstagram, faGithub, faLinkedin, faTelegramPlane, faSpotify, faSteam, faThreads, faDiscord)
+library.add(
+    faXTwitter,
+    faFacebook,
+    faInstagram,
+    faGithub,
+    faLinkedin,
+    faTelegramPlane,
+    faSpotify,
+    faSteam,
+    faThreads,
+    faDiscord
+)
 // Solid
-library.add(faEnvelope)
+library.add(
+    faEnvelope
+)
 </script>
 
 <template>
